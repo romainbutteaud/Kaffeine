4734476ee5c2725eb77edcc07fdbf84655ae0f2d
Romain Butteaud <romain.butteaud@gmail.com>
2014-04-15 00:17:31 +0200

    First commit

---
+++ config/initializers/task_scheduler.rb
@@ -0,0 +1,16 @@
+require 'rufus/scheduler'
+scheduler = Rufus::Scheduler.new
+
+scheduler.every '1m' do
+	require "net/http"
+	require "uri"
+	urls = ['http://followmeback.herokuapp.com',
+			'http://viviio.herokuapp.com',
+			'http://bestyear.herokuapp.com',
+			'http://eifer.herokuapp.com',
+			'http://imaginariumfestival.herokuapp.com'
+			]
+	urls.each do |url|
+		Net::HTTP.get_response(URI.parse(url))
+	end
+end
