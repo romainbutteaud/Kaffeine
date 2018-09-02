744ba61d361dfd35b0ef870935951c1ba999b21b
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2018-09-02 18:11:10 -0300

    require net/http in application

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'net/http'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
